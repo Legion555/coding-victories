b5faeabc2f1df2392c008a9e7b395a6157cae401
Joshua Lausberg <legioncommander555@gmail.com>
2021-03-08 00:36:40 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    env: {
-        MONGODB_URI: 'mongodb+srv://admin:6Yh4HkYI1TcOOyFm@test.qqt89.mongodb.net/coding-victories?retryWrites=true&w=majority',
-        TOKEN_SECRET: 'aks1jf2q4weko'
-    },
-  }
