51a0caa6e8982b79256d0b4ae4cef5c56fab9720
Joshua <legioncommander555@gmail.com>
2021-03-19 22:33:40 +0700

    fix skeleton card

--- components/Default/ListVictories.js
+++ components/Default/ListVictories.js
@@ -18,9 +18,37 @@ export default function ListVictories() {
                 <div className="w-full h-1 bg-gray-400" />
             </div>
             <div className="flex justify-evenly flex-wrap p-4">
-                {victories.map(victory => 
-                    <VictoryCard key={victory._id} victoryData={victory} />
-                )}
+                {!victories ?
+                    victories.map(victory => 
+                        <VictoryCard key={victory._id} victoryData={victory} />
+                    )
+                :
+                <>
+                    <SkeletonCard />
+                    <SkeletonCard />
+                    <SkeletonCard />
+                </>
+                }
+            </div>
+        </div>
+    )
+}
+
+const SkeletonCard = () => {
+    return (
+        <div className="w-10/12 md:w-5/12 lg:w-4/12 xl:w-3/12 h-64 m-4
+            relative rounded-xl shadow text-gray-800">
+            <div className="w-full h-full relative flex flex-col justify-between rounded-xl bg-gray-100 animate-pulse">
+                <div>
+                    <h1 className='w-full h-8 text-2xl bg-gray-400 animate-pulse'></h1>
+                    <p className="h-4 mt-4 mx-4 bg-gray-400 text-xl overflow-y-auto"></p>
+                    <p className="h-4 mt-4 mx-4 bg-gray-400 text-xl overflow-y-auto"></p>
+                    <p className="h-4 mt-4 mx-4 bg-gray-400 text-xl overflow-y-auto"></p>
+                </div>
+                <div className="flex justify-between items-center p-2">
+                    <a className="w-8 h-4 bg-gray-400"></a>
+                    <p className="w-16 h-4 bg-gray-400"></p>
+                </div>
             </div>
         </div>
     )
