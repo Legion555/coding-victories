0e7853a817e5e01552e3077dc048ecb2c8847e4c
Joshua <legioncommander555@gmail.com>
2021-03-19 14:50:41 +0700

    fixed responsive issue

--- components/Authentication/index.js
+++ components/Authentication/index.js
@@ -21,16 +21,16 @@ export default function Index({setViewMain}) {
                     <VictoryCard victoryData={victories[2]} offset={'top-3/4 left-2/4'} animationDur={'7s'} />
                     <VictoryCard victoryData={victories[3]} offset={'bottom-16 left-12'} animationDur={'5s'} />
                 </div>
-                <div className="w-max h-72 px-4 flex items-center text-center relative">
+                <div className="w-max h-max px-4 py-8 flex items-center text-center relative">
                     <div className="w-full h-full bg-blue-400 absolute top-0 left-0
                         rounded-xl shadow transform rotate-6" />
                     <div className="w-full h-full bg-green-400 absolute top-0 left-0
                         rounded-xl shadow transform -rotate-6" />
                     <div className="w-full h-full bg-yellow-400 absolute top-0 left-0 rounded-xl shadow" />
                     <div className="flex flex-col relative">
-                        <h1 className="mb-8 text-3xl lg:text-5xl xl:text-6xl text-gray-700">Welcome to<br/>
-                            <span className="text-4xl lg:text-6xl xl:text-8xl">Coding Victories</span></h1>
-                        <button className="w-max mx-auto p-2 rounded text-2xl text-gray-100 bg-blue-800
+                        <h1 className="mb-8 text-3xl lg:text-4xl xl:text-5xl text-gray-700">Welcome to<br/>
+                            <span className="text-4xl lg:text-5xl xl:text-6xl">Coding Victories</span></h1>
+                        <button className="w-max mx-auto p-2 rounded text-2xl xl:text-3xl text-gray-100 bg-blue-800
                             hover:bg-blue-600 transition ease-in-out duration-150"
                             onClick={() => setViewMain(true)} >View victories</button>
                     </div>
